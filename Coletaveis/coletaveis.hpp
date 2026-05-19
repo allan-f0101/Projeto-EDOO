acadedbfd1a00ec850a0ba98af99a3a0a3186c4d
SuPeR <allandev472@gmail.com>
2026-05-19 16:43:16 -0300

    Coletaveis quase prontos

---
+++ Coletaveis/coletaveis.hpp
@@ -0,0 +1,51 @@
+#ifndef COLETAVEIS_H
+#define COLETAVEIS_H
+
+class Coletaveis{
+    protected:
+        int qtd;
+    public:
+        Coletaveis(int v) : qtd(v) {}
+
+        int getQtd() const {return qtd;}
+        virtual void setQtd(int value){this->qtd = value;};
+
+};
+
+class XP : public Coletaveis{
+    private:
+        int xp;
+    public:
+        XP(int v) : Coletaveis(v), xp(v) {}
+
+        void setQtd(int value) override{
+            this->xp = value;
+        }
+    
+};
+
+class Moeda : public Coletaveis{
+    private:
+        int moeda;
+    
+    public:
+        Moeda(int v) : Coletaveis(v), moeda(v) {}
+
+        void setQtd(int value) override{
+            this->moeda = value;
+        }
+};
+
+class Vida : public Coletaveis{
+    private:
+        int vida;
+    
+    public:
+        Vida(int v) : Coletaveis(v), vida(v) {}
+
+        void setQtd(int value) override{
+            this->vida = value;
+        }
+};
+
+#endif
