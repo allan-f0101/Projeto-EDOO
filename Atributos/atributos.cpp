e74261608f41abdb128935d03cf0f5aa9af74b9a
gustavo-on <oliveiragustavo.nasc@gmail.com>
2026-05-25 20:22:41 -0300

    Adaptação dos arquivos atributos.cpp e atributos.hpp

--- Atributos/atributos.cpp
+++ Atributos/atributos.cpp
@@ -3,146 +3,51 @@
 #include <string>
 #include <vector>
 
-Atributos::Atributos()
-    : vidaMaxima(100),
-      vidaAtual(100),
-      danoBase(10),
-      cooldownTiro(500),
-      velocidade(7),
-      nivel(1),
-      xpAtual(0),
-      xpNecessario(20),
-      semente(12345u)
+Carta::Carta(TipoUpgrade tipo, const std::string& nome, const std::string& descricao)
+    : tipo(tipo), nome(nome), descricao(descricao)
 {}
 
-unsigned int Atributos::proximoAleatorio() const {
+std::ostream& operator<<(std::ostream& os, const Carta& c) {
+    os << "[" << c.nome << "] " << c.descricao;
+    return os;
+}
+
+Catalogo::Catalogo()
+    : semente(12345u)
+{}
+
+unsigned int Catalogo::proximoAleatorio() const {
     semente = semente * 1664525u + 1013904223u;
     return semente;
 }
 
-void Atributos::setVidaAtual(int valor) {
-    if      (valor < 0)          vidaAtual = 0;
-    else if (valor > vidaMaxima) vidaAtual = vidaMaxima;
-    else                         vidaAtual = valor;
+std::vector<Carta> Catalogo::todasAsCartas() const {
+    std::vector<Carta> cartas;
+    cartas.push_back(Carta(TipoUpgrade::DANO,       "Forca Bruta", "+10 de Dano Base"));
+    cartas.push_back(Carta(TipoUpgrade::COOLDOWN,   "Metralhadora", "-10% "));
+    cartas.push_back(Carta(TipoUpgrade::VELOCIDADE, "Pe de Vento", "+1 de Velocidade"));
+    cartas.push_back(Carta(TipoUpgrade::CURA, "Curativo", "+50 de Vida Atual"));
+    cartas.push_back(Carta(TipoUpgrade::VIDA_MAX, "Tanque", "+20 de Vida Maxima e +20 de Vida Atual"));
+    return cartas;
 }
 
-bool Atributos::ganharXp(int quantidade) {
-    xpAtual += quantidade;
+std::vector<Carta> Catalogo::sortear3() const {
+    std::vector<Carta> cartas = todasAsCartas();
 
-    if (xpAtual >= xpNecessario) {
-        xpAtual -= xpNecessario;
-        xpNecessario = static_cast<int>(xpNecessario * 1.5f);
-        nivel++;
-        std::vector<TipoUpgrade> opcoes = sortearUpgrades();
-
-        std::cout << "[Level Up] Nivel" << nivel << " atingido! Upgrades sorteados:\n";
-        for (int i = 0; i < static_cast<int>(opcoes.size()); ++i) {
-            std::cout << " [" << (i + 1) << "] "
-                      << nomeUpgrade(opcoes[i])
-                      << " -> " << descricaoUpgrade(opcoes[i]) << "\n";
-        }
-
-        aplicarUpgrade(opcoes[0]);
-        std::cout << "[Auto] Upgrade aplicado: "
-                  << nomeUpgrade(opcoes[0]) << "\n";
-        return true;
-    }
-
-    return false;
-}
-
-std::vector<TipoUpgrade> Atributos::sortearUpgrades() const {
-    TipoUpgrade catalogo[5] = {
-        TipoUpgrade::FORCA_BRUTA,
-        TipoUpgrade::METRALHADORA,
-        TipoUpgrade::PE_DE_VENTO,
-        TipoUpgrade::CURATIVO,
-        TipoUpgrade::TANQUE
-    };
-
-    for (int i = 4; i > 0; --i) {
+    for (int i = static_cast<int>(cartas.size()) - 1; i > 0; --i) {
         int j = static_cast<int>(proximoAleatorio() % static_cast<unsigned int>(i + 1));
-        TipoUpgrade tmp = catalogo[i];
-        catalogo[i]     = catalogo[j];
-        catalogo[j]     = tmp;
+        Carta tmp = cartas[i];
+        cartas[i] = cartas[j];
+        cartas[j] = tmp;
     }
-
-    std::vector<TipoUpgrade> resultado;
-    resultado.push_back(catalogo[0]);
-    resultado.push_back(catalogo[1]);
-    resultado.push_back(catalogo[2]);
-    return resultado;
+    return std::vector<Carta>(cartas.begin(), cartas.begin() + 3);
 }
 
-void Atributos::aplicarUpgrade(TipoUpgrade upgrade) {
-    switch (upgrade) {
-        case TipoUpgrade::FORCA_BRUTA:
-            danoBase += 10;
-            break;
-        case TipoUpgrade::METRALHADORA:
-            cooldownTiro = static_cast<int>(cooldownTiro * 0.9f);
-            break;
-        case TipoUpgrade::PE_DE_VENTO:
-            velocidade += 1;
-            break;
-        case TipoUpgrade::CURATIVO:
-            setVidaAtual(vidaAtual + 50);
-            break;
-        case TipoUpgrade::TANQUE:
-            vidaMaxima += 20;
-            vidaAtual  += 20;   
-            break;
+Carta Catalogo::buscarCarta(TipoUpgrade tipo) const {
+    std::vector<Carta> todas = todasAsCartas();
+    for (int i = 0; i < static_cast<int>(todas.size()); ++i) {
+        if (todas[i].getTipo() == tipo)
+            return todas[i];
     }
-}
-
-std::string Atributos::nomeUpgrade(TipoUpgrade upgrade) const {
-    switch (upgrade) {
-        case TipoUpgrade::FORCA_BRUTA:  return "Forca Bruta";
-        case TipoUpgrade::METRALHADORA: return "Metralhadora";
-        case TipoUpgrade::PE_DE_VENTO:  return "Pe de Vento";
-        case TipoUpgrade::CURATIVO:     return "Curativo";
-        case TipoUpgrade::TANQUE:       return "Tanque";
-    }
-    return "Desconhecido";
-}
-
-std::string Atributos::descricaoUpgrade(TipoUpgrade upgrade)
-const {
-    auto intStr = [](int n) -> std::string {
-        if (n == 0) return "0";
-        std::string s;
-        bool neg = (n < 0);
-        if (neg) n = -n;
-        while (n > 0) { s = static_cast<char>('0' + n % 10) + s; n /= 10;}
-        return neg ? "-" + s : s;
-    };
-
-    switch (upgrade) {
-        case TipoUpgrade::FORCA_BRUTA:
-            return "Dano Base: " + intStr(danoBase) + " -> " + intStr(danoBase + 10);
-        case TipoUpgrade::METRALHADORA:
-            return "Cooldown: " + intStr(cooldownTiro) + "ms -> " + intStr(static_cast<int>(cooldownTiro * 0.9f)) + "ms";
-        case TipoUpgrade::PE_DE_VENTO:
-            return "Velocidade: " + intStr(velocidade) + " -> " + intStr(velocidade + 1);
-        case TipoUpgrade::CURATIVO: {
-            int apos = (vidaAtual + 50 > vidaMaxima) ? vidaMaxima : vidaAtual + 50;
-            return "Vida: " + intStr(vidaAtual) + " -> " + intStr(apos)
-                            + " (max " + intStr(vidaMaxima) + ")";
-        }
-        case TipoUpgrade::TANQUE:
-            return "Vida Max: " + intStr(vidaMaxima) + " -> " + intStr(vidaMaxima + 20) + " | Vida: +" + intStr(20);
-    }
-    return "";
-}
-
-std::ostream& operator<<(std::ostream& os, const Atributos& a) {
-    os << "=== ATRIBUTOS ===\n"
-       << "  Nivel       : " << a.nivel
-       << "  (XP: " << a.xpAtual << "/" << a.xpNecessario << ")\n"
-       << "  Vida        : " << a.vidaAtual << "/" << a.vidaMaxima << "\n"
-       << "  Dano Base   : " << a.danoBase  << "\n"
-       << "  Cooldown    : " << a.cooldownTiro << "ms\n"
-       << "  Velocidade  : " << a.velocidade  << "\n"
-       << "=================\n";
-    return os;
+    return todas[0];
 }
--- Atributos/atributos.hpp
+++ Atributos/atributos.hpp
@@ -1,56 +1,39 @@
 #ifndef INIMIGOS_HPP
 #define INIMIGOS_HPP
 
+#include "jogador.hpp"
 #include <string>
 #include <vector>
 #include <iostream>
 
-enum class TipoUpgrade {
-    FORCA_BRUTA,   // +10 Dano Base
-    METRALHADORA,  // Cooldown * 0.9
-    PE_DE_VENTO,   // +1 Velocidade
-    CURATIVO,      // +50 Vida Atual (limitado à Vida Máxima)
-    TANQUE         // +20 Vida Máxima e +20 Vida Atual
+class Carta {
+    private:
+        TipoUpgrade tipo;
+        std::string nome;
+        std::string descricao;
+    public:
+        Carta(TipoUpgrade tipo, const std::string& nome, const std::string& descricao);
+
+        TipoUpgrade getTipo()       const {return tipo;}
+        std::string getNome()       const {return nome;}
+        std::string getDescricao()  const {return descricao;}
+
+        friend std::ostream& operator<<(std::ostream& os, const Carta& c);
 };
 
-class Atributos {
+std::ostream& operator<<(std::ostream& os, const Carta& c);
+
+class Catalogo {
     private:
-        int vidaMaxima;
-        int vidaAtual;
-        int danoBase;
-        int cooldownTiro;
-        int velocidade;
-
-        int nivel;
-        int xpAtual;
-        int xpNecessario;
-
         mutable unsigned int semente;
 
-        unsigned int                proximoAleatorio() const;
-        std::vector<TipoUpgrade>    sortearUpgrades() const;
-        void                        aplicarUpgrade(TipoUpgrade upgrade);
-        std::string                 nomeUpgrade(TipoUpgrade upgrade) const;
-        std::string                 descricaoUpgrade(TipoUpgrade upgrade) const;
-
+        unsigned int proximoAleatorio() const;
+        std::vector<Carta> todasAsCartas() const;
+    
     public:
-        Atributos();
-        int getVidaMaxima()   const { return vidaMaxima;   }
-        int getVidaAtual()    const { return vidaAtual;    }
-        int getDanoBase()     const { return danoBase;     }
-        int getCooldownTiro() const { return cooldownTiro; }
-        int getVelocidade()   const { return velocidade;   }
-        int getNivel()        const { return nivel;        }
-        int getXpAtual()      const { return xpAtual;      }
-        int getXpNecessario() const { return xpNecessario; }
-
-        void setVidaAtual(int valor);
-
-        bool ganharXp(int quantidade);
-
-        friend std::ostream& operator<<(std::ostream& os, const Atributos& a);
+        Catalogo();
+        std::vector<Carta> sortear3() const;
+        Carta buscarCarta(TipoUpgrade tipo) const;
 };
-
-std::ostream& operator<<(std::ostream& os, const Atributos& a);
 
 #endif
